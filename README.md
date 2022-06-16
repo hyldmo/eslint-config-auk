4043fa84653f7f15189cb341077e5bea5b3c1e3f
Eivind <eivindh3@gmail.com>
2022-06-16 10:14:12 +0200

    feat: added eslint and release config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # eslint-config-auk
+
+Opinionated ESLint config used by Auk's React projects
---
+++ index.js
@@ -0,0 +1,90 @@
+const enabled = 'error' // Used to set error level across rules
+
+module.exports = {
+	env: {
+		browser: true,
+		node: true
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 'latest'
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	rules: {
+		'@typescript-eslint/array-type': [enabled, { default: 'array-simple' }],
+		'@typescript-eslint/consistent-type-definitions': [enabled, 'interface'], // See https://github.com/microsoft/TypeScript/wiki/Performance#preferring-interfaces-over-intersections
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': [enabled, { argsIgnorePattern: '_' }],
+		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/prefer-for-of': enabled,
+		'@typescript-eslint/prefer-function-type': enabled,
+		'@typescript-eslint/unified-signatures': enabled,
+		'react/self-closing-comp': enabled,
+		'react/jsx-curly-brace-presence': enabled,
+
+		'arrow-parens': ['off', 'as-needed'],
+		'constructor-super': enabled,
+		'dot-notation': enabled,
+		'guard-for-in': enabled,
+		'id-blacklist': [enabled, 'any', 'number', 'String', 'string', 'Boolean', 'boolean'],
+		'id-match': enabled,
+		'linebreak-style': [enabled, 'unix'],
+		'max-classes-per-file': [enabled, 1],
+		'new-parens': enabled,
+		'no-bitwise': enabled,
+		'no-caller': enabled,
+		'no-cond-assign': enabled,
+		'no-console': [enabled, { allow: ['info', 'warn', 'error', 'debug'] }],
+		'no-debugger': enabled,
+		'no-duplicate-case': enabled,
+		'no-empty': enabled,
+		'no-eval': enabled,
+		'no-extra-bind': enabled,
+		'no-fallthrough': 'off',
+		'no-invalid-this': 'off',
+		'no-mixed-spaces-and-tabs': 'off', // Not needed with prettier on
+		'no-multiple-empty-lines': [enabled, { max: 1 }],
+		'no-new-func': enabled,
+		'no-new-wrappers': enabled,
+		'no-redeclare': 'off', // Uses typescripts version
+		'no-restricted-imports': [enabled],
+		'no-return-await': enabled,
+		'no-sequences': enabled,
+		'no-shadow': [enabled, { hoist: 'all' }],
+		'no-sparse-arrays': enabled,
+		'no-template-curly-in-string': enabled,
+		'no-throw-literal': enabled,
+		'no-undef-init': enabled,
+		'no-unsafe-finally': enabled,
+		'object-shorthand': enabled,
+		'one-var': [enabled, 'never'],
+		'prefer-arrow-callback': enabled,
+		'prefer-object-spread': enabled,
+		'prefer-template': enabled,
+		'quote-props': [enabled, 'consistent-as-needed'],
+		'react/prop-types': 'off',
+		'space-in-parens': enabled,
+		'spaced-comment': [enabled, 'always', { markers: ['/'] }],
+		'use-isnan': enabled,
+
+		// DEFAULTS: remove these
+		'@typescript-eslint/no-empty-interface': enabled,
+		'@typescript-eslint/no-redeclare': enabled,
+		'@typescript-eslint/ban-ts-comment': enabled,
+		'@typescript-eslint/ban-types': enabled,
+		'@typescript-eslint/no-var-requires': enabled,
+		'prefer-const': enabled
+	}
+}
